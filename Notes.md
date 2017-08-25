5d43ff4cb8da8ff57d34a5b6500ab97ae4ba6b5e
Chris <cfu6@binghamton.edu>
2017-08-25 08:22:28 -0400

    Init

---
+++ Notes.md
@@ -0,0 +1,11 @@
+# Ecology
+## 8-23 Notes
+* Office hours S3 384 after lectures on Monday
+* Ecology - The economy of nature - Launchpad is not required
+* Exams 75%, Final cumulative but optional
+* Multiple choice short answer
+* Assignments are 25 points
+* Participation is 15% of grade - participation
+* My courses assignments - post response to reading or video
+* Iclicker.com through mycourses -tools -register -put in code in back of iclicker
+* (HW) read syl and chap 1
---
+++ ProcessNotes.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# Create flash cards csv from notes for import to anki and point out things to
+# review (?) and HW items (HW) before creating flash cards
+import re, sys, csv
+
+fileName = ""
+
+try:
+  fileName = sys.argv[1]
+except IndexError:
+  print("No file provided, using Notes.md")
+  fileName = "Notes.md"
+
+with open(fileName, 'r', encoding="utf-8") as file:
+    lines_list = file.readlines()
+
+hwList = []
+
+for line in lines_list:
+    #Check for (?), dont make flashcards if these exist
+    n = re.match(r'.*(\(HW\)).*' , line)
+    m = re.match(r'.*\~\~(\(HW\))\~\~.*' , line)
+    if (m != None): #skip hw if crossed out
+        pass
+    elif (n != None):
+        hwList.append(line)
+if len(hwList) != 0:
+    print("Homework: ")
+    for item in hwList:
+        print (item)
+else:
+    print("No Homework.")
+
+qlist = []
+
+for line in lines_list:
+    #Check for (?), dont make flashcards if these exist
+    n = re.match(r'.*(\(\?\)).*' , line)
+    if (n != None):
+        qlist.append(line)
+
+if len(qlist) != 0:
+    print("Questions to resolve: ")
+    for item in qlist:
+        print (item)
+else:
+    print("No questions to resolve.")
+    with open('Flashcards/output.csv', 'w', newline='') as csvfile:
+      writer = csv.writer(csvfile, delimiter='|',quotechar='|')
+      for line in lines_list:
+        #Pull out bolded terms and their definitions from the file
+        m = re.match(r'\s*\**.*\*\*(.*)\*\*\s*\-\s*(.*)\s' , line)
+        if m != None:
+            writer.writerow([m.group(1),m.group(2)])
+            #print(m.groups())
+
+print("Done")
